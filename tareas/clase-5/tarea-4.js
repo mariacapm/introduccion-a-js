f8d53ab602d57b83921db48bfa0f4d81c17177d2
Maria Capurro <mariacapm@gmail.com>
2022-04-28 20:28:20 +0200

    Crea función mostrarElementosOcultos
    Para poder cambiar a visibility: visible en los textos de los cálculos.

--- tareas/clase-5/tarea-4.js
+++ tareas/clase-5/tarea-4.js
@@ -6,3 +6,7 @@
 // 3. obtener el número más grande y mostrarlo en un <em> pre-creado con el texto "El número más grande es..."
 // 4. obtener el número que más se repite y mostrarlo en un <em> pre-creado con el texto "El número más frecuente es..."
 
+function mostrarElementosOcultos() {
+  const elementosOcultos = document.querySelectorAll(".hidden");
+  elementosOcultos.forEach((em) => (em.style.visibility = "visible"));
+}
