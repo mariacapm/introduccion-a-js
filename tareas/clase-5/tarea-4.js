250e7bb452ee14cfe62780ddb05375b6aeb91524
Maria Capurro <mariacapm@gmail.com>
2022-04-28 20:29:38 +0200

    Crea función para ingresar datos
    Commit para que el usuario pueda ingresar los valores que luego se mostrarán en la lista, que luego se guardarán en un Array y con los que posteriormente se harán los cálculos que pide la tarea.

--- tareas/clase-5/tarea-4.js
+++ tareas/clase-5/tarea-4.js
@@ -6,6 +6,37 @@
 // 3. obtener el número más grande y mostrarlo en un <em> pre-creado con el texto "El número más grande es..."
 // 4. obtener el número que más se repite y mostrarlo en un <em> pre-creado con el texto "El número más frecuente es..."
 
+let arrayNumeros = [];
+
+function ingresarNumeros() {
+  let pregunta;
+  let crearLista = document.querySelector("ol");
+
+  function preguntar() {
+    pregunta = prompt(
+      "Vamos a crear una lista de números. Ingresa un número y haz clic a aceptar, luego ingresa otro número, y así sucesivamente. Cuando hayas terminado, haz clic en Cancelar"
+    );
+  }
+  preguntar();
+  
+  if (isNaN(pregunta) || pregunta == null || pregunta.trim().length === 0) {
+    alert('El valor ingresado no es un número');
+  } else {
+    while (!isNaN(pregunta) && pregunta !== null && pregunta.trim().length !== 0) {
+      let number = Number(pregunta);
+      arrayNumeros.push(number);
+
+      let elementoLista = document.createElement("li");
+      crearLista.appendChild(elementoLista);
+      elementoLista.innerText = number;
+
+      preguntar();
+    }
+    mostrarElementosOcultos();
+  }
+}
+
+
 function mostrarElementosOcultos() {
   const elementosOcultos = document.querySelectorAll(".hidden");
   elementosOcultos.forEach((em) => (em.style.visibility = "visible"));
