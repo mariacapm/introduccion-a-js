eadbefe3cbe2222e14aca748a6a150cf5b8b72cc
Maria Capurro <mariacapm@gmail.com>
2022-05-03 23:30:55 +0200

    Crea funcion .onclick para el formulario
    Para poder guardar en un array el numero que ingresa el usuario en el formulario

--- tareas/clase-5/tarea-4.js
+++ tareas/clase-5/tarea-4.js
@@ -6,25 +6,15 @@
 // 3. obtener el número más grande y mostrarlo en un <em> pre-creado con el texto "El número más grande es..."
 // 4. obtener el número que más se repite y mostrarlo en un <em> pre-creado con el texto "El número más frecuente es..."
 
+let numero;
 let arrayNumeros = [];
 
-function ingresarNumeros() {
-  let pregunta;
-  let crearLista = document.querySelector("ol");
-
-  function preguntar() {
-    pregunta = prompt(
-      "Vamos a crear una lista de números. Ingresa un número y haz clic a aceptar, luego ingresa otro número, y así sucesivamente. Cuando hayas terminado, haz clic en Cancelar"
-    );
-  }
-  preguntar();
-  
-  if (isNaN(pregunta) || pregunta == null || pregunta.trim().length === 0) {
-    alert('El valor ingresado no es un número');
-  } else {
-    while (!isNaN(pregunta) && pregunta !== null && pregunta.trim().length !== 0) {
-      let number = Number(pregunta);
-      arrayNumeros.push(number);
+document.querySelector("#boton-enviar").onclick = function enviar() {
+  guardarNumero();
+  crearElementoLista();
+  ingresarNumerosArray();
+  return false;
+};
 
       let elementoLista = document.createElement("li");
       crearLista.appendChild(elementoLista);
