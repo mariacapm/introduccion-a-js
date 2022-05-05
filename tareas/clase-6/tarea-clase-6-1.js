5f094012e0bf08bae201da2701ad190b005eee54
Maria Capurro <mariacapm@gmail.com>
2022-05-05 21:31:43 +0200

    Crea función crearLabel
    Para que aparezca un label por cada input por cantidad de miembros de la familia que se determinen.

--- tareas/clase-6/tarea-clase-6-1.js
+++ tareas/clase-6/tarea-clase-6-1.js
@@ -5,3 +5,12 @@ Al hacer click en "calcular", mostrar en un elemento pre-existente la mayor edad
 
 Punto bonus: Crear un botón para "empezar de nuevo" que empiece el proceso nuevamente, borrando los inputs ya creados (investigar cómo en MDN).
 */
+function crearLabel(i) {
+  const nodoLabel = document.createElement("label");
+  nodoLabel.htmlFor = "persona" + i;
+  nodoLabel.innerText = "Edad Integrante " + i + ":";
+  document.querySelector("form").appendChild(nodoLabel);
+  const saltoLinea = document.createElement("br");
+  nodoLabel.before(saltoLinea);
+}
+
